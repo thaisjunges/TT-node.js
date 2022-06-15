e60c1d264081297b578580b181a28461a34aa290
thaisjunges <ferreira.thais98@hotmail.com>
2022-06-14 22:15:33 -0300

    aula02

---
+++ aula02/currentDate.js
@@ -0,0 +1,12 @@
+//currentDate.js
+export function currentDate() {
+    return new Date()
+}
+
+export function bomDia() {
+    return 'Bom Dia'
+}
+
+
+//exportou a funcao currentDate para ser usada externamente por outros arquivos
+//export default currentDate
---
+++ aula02/myName.js
@@ -0,0 +1,5 @@
+function name() {
+    return 'Thais F.J'
+}
+
+export default name
