c6ba7820196c7a7929bf59f85e7cd30ca39fbd14
thaisjunges <ferreira.thais98@hotmail.com>
2022-07-05 22:09:44 -0300

    Aula 08: fix de conexao, rota de busca e insercao

--- aula08/api-node/server.js
+++ aula08/api-node/server.js
@@ -1,9 +1,6 @@
 import express from "express"
 import bodyParser from 'body-parser'
 import con from './connection.js'
-import { createRequire } from "module"; // Bring in the ability to create the 'require' method
-const require = createRequire(import.meta.url); // construct the require method
-const mockDepartamentos = require("./mock/departamentos.mock.json") // use the require method
 
 const app = express()
 
@@ -16,13 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 // Lista os departamentos
 app.get('/departamentos', (req, res) => {
-   con.connect((err) => {
-    if (err) {
-        throw err
-    }
-   })
-
-   con.query('SELECT * FROM DEPARTAMENTOS ORDER BY nome', (err, result) => {
+   con.query('SELECT sigla FROM DEPARTAMENTOS ORDER BY nome', (err, result) => {
     res.send(result)
    })
 })
@@ -31,22 +22,22 @@ app.get('/departamentos', (req, res) => {
 app.get('/departamentos/:idDepartamento', (req, res) => {
     const {idDepartamento} = req.params
 
-    const resultado = mockDepartamentos.filter((depto) => {
-        return depto.id == idDepartamento
+    //Implemente uma query que retore o departamento conforme o ID passado na rota
+    con.query(`SELECT * FROM DEPARTAMENTOS WHERE id_departamento = ${idDepartamento}`, (err, result) => {
+        res.send(result)
+      })
     })
-
-    res.send(resultado)
-})
 
 // Insere um departamento
 app.post('/departamentos', (req, res) => {
     const { nome, sigla } = req.body
     //Antes vamos validar se os dados vieram corretamente
     if (nome != undefined && sigla != undefined) {
+        //Implemente a inserçao do Depto. no banco de dados
+        con.query(`INSERT INTO FROM DEPARTAMENTOS = ${nome, sigla} SET`), (err, result) => {
+            res.send(result)
+        }
         //se tudo estiver ok inserir no banco
-     res.send(`Insere departamento: ${nome} - ${sigla}`)   
-    } else {
-        res.send('Algo deu errado')
     }
 
     
